3acabb1dcf89e79665d67ae7215391e7786e9670
Katherine Lewis <kale8432@colorado.edu>
2019-07-11 09:47:54 -0600

    add components and methods to app to add and delete idea

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../Form/Form';
+import Container from '../Container/Container';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
+	constructor() {
+		super();
+		this.state = {
+			ideas: []
+		};
+	}
 
-        </div>
-        <div className='resy-container'>
-          
-        </div>
-      </div>
-    )
-  }
+	componentDidMount() {
+		fetch('').then(response => response.json()).then(ideas =>
+			this.setState({
+				ideas
+			})
+		);
+	}
+
+	addIdea = newIdea => {
+		this.setState({
+			ideas: [ ...this.state.ideas, newIdea ]
+		});
+	};
+
+	deleteIdea = id => {
+		const filteredIdeas = ideas.filter(idea => idea.id != id);
+		this.setState({
+			ideas: filteredIdeas
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1 className="app-title">Turing Cafe Reservations</h1>
+				<div className="resy-form">
+					<Form addIdea={this.addIdea} />
+				</div>
+				<div className="resy-container">
+					<Container deleteIdea={this.deleteIdea} ideas={this.state.ideas} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
