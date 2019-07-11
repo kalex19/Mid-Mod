566b3d2cfc1a780a7e361a0d1fe2d10a727d65e0
Katherine Lewis <kale8432@colorado.edu>
2019-07-11 10:11:56 -0600

    styled reservation cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,12 +3,14 @@ import './Card.css';
 
 export default function Card({ name, id, date, time, number, deleteIdea }){
 	return (
-		<div>
-			<h1>`${name}'/s Reservation</h1>
-			<p>{date}</p>
-			<p>{time}</p>
+		<div className="card">
+			<h1>{name}'s Reservation</h1>
+			<p>Date: {date}</p>
+			<p>Time: {time}</p>
 			<p>{number}</p>
-			<button onClick={() => deleteIdea(id)}>Cancel Reservation</button>
+			<button className="deleteBtn" onClick={() => deleteIdea(id)}>
+				Cancel Reservation
+			</button>
 		</div>
 	);
 }
